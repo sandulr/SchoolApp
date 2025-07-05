5bbec83a3130a6341f87292dbd9df943a7f89326
sandulr <sandulr2@gmail.com>
2025-07-05 19:01:13 +0000

    before night commit

--- app/src/main/java/com/example/schoolapp/SecondPage.java
+++ app/src/main/java/com/example/schoolapp/SecondPage.java
@@ -1,8 +1,6 @@
 package com.example.schoolapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void moveSecondPage(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondPage.class);
-        startActivity(intent);
     }
 }
